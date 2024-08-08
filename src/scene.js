a0d87e4e888107630a831320aff8c363319a15b2
Brady Kelly <brady691213@outlook.com>
2024-08-08 23:49:08 +0200

    Moving left and right but off by one.

--- src/scene.js
+++ src/scene.js
@@ -1,7 +1,8 @@
+
 class PacmanTileScene extends Phaser.Scene {
 
     cellSize = 32;
-    currCell = { x: 1, y: 1 };
+    currCell = { x: 1, y: 1  };
     pacRadius = 30;
 
     preload() {
@@ -13,27 +14,17 @@ class PacmanTileScene extends Phaser.Scene {
     }
 
     create() {
-        const map = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
-        const tileset = map.addTilesetImage('tiles', null, 32, 32, 1, 2);
+        const map = this.make.tilemap({ key: 'map', tileWidth: this.cellSize, tileHeight: this.cellSize });
+        const tileset = map.addTilesetImage('tiles', null, this.cellSize, this.cellSize, 1, 2);
         const layer = map.createLayer(0, tileset, 0, 0);
 
-        const player = this.add.image(32 + 16, 32 + 16, 'car');
-
-        this.pacman = this.createPacman(this.currCell, this.cellSize / 2 - 2);
-
-
+        // Makeppac
+        this.pacman = this.createPacman(this.currCell, this.cellSize);
     }
 
-    createPacman(cell, radius) {
+    createPacman(cell, cellSize) {
 
         const pg = this.add.graphics();
-        const pp = this.physics.add.existing(pg);
-        // pg.body.velocity.x = 100;
-        // pg.body.velocity.y = 200;
-        pg.body.allowGravity = false;
-        pg.body.bounce.x = 1;
-        pg.body.bounce.y = 1;
-        pg.body.collideWorldBounds = true;
         this.tweens.addCounter({
             from: 0,
             to: 30,
@@ -42,15 +33,13 @@ class PacmanTileScene extends Phaser.Scene {
             repeat: -1,
             onUpdate: function (tween)
             {
-                const x = (cell.x * radius * 2) + ((cell.x - 1) * 4) + radius + 4;
-                const y = (cell.y * radius * 2) + ((cell.y - 1) * 4) + radius + 4;
+                const x = cell.x * cellSize + (cellSize / 2);
+                const y = cell.y * cellSize + (cellSize / 2);
                 const t = tween.getValue();
                 pg.clear();
                 pg.fillStyle(0xffff00, 1);
                 pg.slice(x, y, radius, Phaser.Math.DegToRad(330 + t), Phaser.Math.DegToRad(30 - t), true);
                 pg.fillPath();
-                pg.fillStyle(0x000000, 1);
-                pg.fillEllipse(radius + radius / 3, radius / 3,  radius / 3,  radius / 3);
             }
         });
         return pg;
